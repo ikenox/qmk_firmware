f359a901daa0b237e65ff69eb18a00fb6cccda21
Naoto Ikeno <ikenox@gmail.com>
2018-12-25 22:32:45 +0900

    fix broken codes by rebase

--- keyboards/crkbd/keymaps/ikeno/keymap.c
+++ keyboards/crkbd/keymaps/ikeno/keymap.c
@@ -52,7 +52,7 @@ enum macro_keycodes {
 #define KC_LSAD  RGB_SAD
 #define KC_LVAI  RGB_VAI
 #define KC_LVAD  RGB_VAD
-#define KC_LSMOD RGB_SMOD
+#define KC_LSMOD RGB_MOD
 #define KC_CTLTB CTL_T(KC_TAB)
 #define KC_GUIEI GUI_T(KC_LANG2)
 #define KC_ALTKN ALT_T(KC_LANG1)
