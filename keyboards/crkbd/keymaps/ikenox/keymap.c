7c93aa5c0009313c25410d6ce0e736a33111ccb4
Naoto Ikeno <ikenox@gmail.com>
2018-12-29 04:38:59 +0900

    update

---
+++ keyboards/crkbd/keymaps/ikenox/keymap.c
@@ -0,0 +1,171 @@
+#include QMK_KEYBOARD_H
+#ifdef PROTOCOL_LUFA
+  #include "split_util.h"
+#endif
+#ifdef SSD1306OLED
+  #include "oled.h"
+#endif
+
+#include "edvorakjp.h"
+
+/*
+ * enum custom_keycodes {
+ *   KC_LOCK = NEW_SAFE_RANGE,
+ * };
+ */
+
+#define KC_ KC_TRNS
+
+#define KC_TMB1 LGUI_T(KC_TAB)
+#define KC_TMB2 LSFT_T(KC_SPC)
+#define KC_TMB3 TD(TD_LOWER) // act as LOWER when hold, as KC_LANG2(=English) when tapped
+#define KC_TMB4 TD(TD_RAISE) // act as RAISE when hold, as KC_LANG1(=Japanese) when tapped
+#define KC_TMB5 RCTL_T(KC_BSPC)
+#define KC_TMB6 RALT_T(KC_ENT)
+#define KC_TMB7 KC_DEL
+#define KC_TMB8 RALT(KC_ENT)
+#define KC_TMB9 LGUI(KC_TAB)
+#define KC_CTLTB CTL_T(KC_TAB)
+#define KC_GUIEI GUI_T(KC_LANG2)
+#define KC_GUIKN GUI_T(KC_LANG1)
+#define KC_CTLSP LCTL(KC_SPC)
+
+#define KC_RAISE TD(TD_RAISE)
+#define KC_LOWER TD(TD_LOWER)
+
+#define KC_RAENT RALT_T(KC_ENT)
+#define KC_LASPC LALT_T(KC_SPC)
+
+#define KC_RSBSL RSFT_T(KC_BSLS)
+#define KC_LSSLS LSFT_T(KC_SLSH)
+
+#define KC_RST  RESET
+#define KC_DBUG DEBUG
+#define KC_RTOG RGB_TOG
+#define KC_EDJP EDVORAK
+#define KC_QWER QWERTY
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_EDVORAK] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     ESC ,QUOT,COMM,DOT , P  , Y  ,             F  , G  , C  , R  , W  ,BSLS,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+    CTLTB, A  , O  , E  , I  , U  ,             D  , M  , T  , N  , S  ,MINS,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     LSFT,SCLN, Q  , Z  , V  , X  ,             B  , H  , J  , K  , L  ,LSSLS,
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                      GUIEI,RAISE,LASPC,  RAENT,LOWER,GUIKN \
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_EDVORAKJ1] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         , AI , OU , EI ,    ,    ,                ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,                ,    ,    , Y  ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,ANN ,ONN ,ENN ,INN ,UNN ,                ,    ,    ,    ,    ,    ,
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                             ,    ,    ,      ,    ,
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_EDVORAKJ2] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         , AI , OU , EI ,    ,    ,                ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,                , Y  ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,ANN ,ONN ,ENN ,INN ,UNN ,                ,    ,    ,    ,    ,    ,
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                             ,    ,    ,      ,    ,
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_QWERTY] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     TAB , Q  , W  , E  , R  , T  ,             Y  , U  , I  , O  , P  ,MINS,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+    CTLTB, A  , S  , D  , F  , G  ,             H  , J  , K  , L  ,SCLN,QUOT,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     LSFT, Z  , X  , C  , V  , B  ,             N  , M  ,COMM,DOT ,SLSH,RSBSL,
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                      GUIEI,RAISE,LASPC,  RAENT,LOWER,GUIKN \
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_LOWER] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     TILD,EXLM, AT ,HASH, DLR,PERC,            AMPR,ASTR,LPRN,LCBR,LBRC,UNDS,\
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+      GRV, 1  , 2  , 3  , 4  , 5  ,            PLUS, EQL,RPRN,RCBR,RBRC,PIPE,\
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     CIRC, 6  , 7  , 8  , 9  , 0  ,                ,BSPC, LT , GT ,    ,    ,\
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                      GUIEI,RAISE,LASPC,  RAENT,LOWER,GUIKN \
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_RAISE] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     TILD,EXLM, AT ,HASH, DLR,PERC,            AMPR,ASTR,LPRN,LCBR,LBRC,UNDS,\
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+      GRV, 1  , 2  , 3  , 4  , 5  ,            PLUS, EQL,RPRN,RCBR,RBRC,PIPE,\
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+     CIRC, 6  , 7  , 8  , 9  , 0  ,                ,BSPC, LT , GT ,    ,    ,\
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                      GUIEI,RAISE,LASPC,  RAENT,LOWER,GUIKN \
+  //                    `----+----+----' `----+----+----'
+  ),
+
+  [_ADJUST] = LAYOUT_kc(
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,    ,    ,   ,EXTOFF,   ,                ,EXTON,   ,    ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,    ,    ,QWER,WIN ,RST ,            RTOG,MAC ,EDJP,    ,    ,    ,
+  //|----+----+----+----+----+----|           |----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,                ,    ,    ,    ,    ,    ,
+  //`----+----+----+----+----+----+----| |----+----+----+----+----+----+----'
+                             ,    ,    ,      ,    ,
+  //                    `----+----+----' `----+----+----'
+  )
+};
+
+#ifdef SSD1306OLED
+void matrix_init_keymap(void) {
+  //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+  iota_gfx_init(!has_usb());   // turns on the display
+}
+
+void matrix_scan_user(void) {
+  iota_gfx_task();  // this is what updates the display continuously
+}
+#endif
+
+#ifdef RGBLIGHT_EFFECT_STATIC_GRADIENT
+uint32_t layer_state_set_keymap(uint32_t state) {
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+  switch (biton32(state)) {
+    case _EDVORAKJ1:
+    case _EDVORAKJ2:
+      // _EDVORAKJ1 & J2 are same colored
+      rgblight_sethsv_noeeprom_white();
+      break;
+    case _LOWER:
+      rgblight_sethsv_noeeprom_red();
+      break;
+    case _RAISE:
+      rgblight_sethsv_noeeprom_blue();
+      break;
+    case _ADJUST:
+      rgblight_sethsv_noeeprom_green();
+      break;
+    default: //  for any other layers, or the default layer
+      rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
+      rgblight_sethsv_noeeprom_red();
+      break;
+  }
+  return state;
+}
+#endif
